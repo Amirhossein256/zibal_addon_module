339461bf6c00d6fecd86b18d528c4057e4005ca4
alireza <alireza.abbasnezhad@outlook.com>
2024-03-23 02:22:35 +0330

    add update setting method

--- src/controller/zibal.php
+++ src/controller/zibal.php
@@ -1,9 +1,33 @@
 <?php
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 class zibal extends mainController
 {
     public function setting()
     {
-        $this->render('setting');
+
+        $setting = $this->getSetting();
+        $this->render('setting', ['setting' => $setting]);
+    }
+
+    public function settingUpdate()
+    {
+        if (!empty($_POST)){
+            foreach ($_POST as $key=>$value){
+                Capsule::table('zibal_settings')->where('key', $key)->update(['value' => $value]);
+            }
+
+
+        }else{
+            $this->response('اطلاعات وارد شده صحبح نمیباشد','422');
+        }
+        $setting = $this->getSetting();
+        $this->render('setting', ['setting' => $setting]);
+    }
+
+    public function getSetting()
+    {
+        return Capsule::table('zibal_settings')->pluck('value', 'key');
     }
 }
