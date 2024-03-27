e2b9c11a37f65f474e41a56e4cfd1cb34adba1ef
alireza <alireza.abbasnezhad@outlook.com>
2024-03-27 13:03:22 +0330

    edit hook name and add btn && prepare for request

--- hooks.php
+++ hooks.php
@@ -1,3 +1,4 @@
 <?php
 
-require_once __DIR__ . '/src/hooks/showMobileInProfile.php';
+require_once __DIR__ . '/src/hooks/authenticateInProfile.php';
+require_once __DIR__ . '/src/hooks/ClientAreaPage.php';
