b4ba61bb5f92f20e6fc194b7931ab66feebf8695
alireza <alireza.abbasnezhad@outlook.com>
2024-03-28 13:11:57 +0330

    add navbar for admin area

--- src/views/setting.php
+++ src/views/setting.php
@@ -1,5 +1,47 @@
-<hr>
-<h1 style="direction: rtl"> تنظیمات ماژول احراز هویت</h1>
+<style>
+    /* Custom styles for this template */
+    .navbar-mini {
+        background-color: #f8f9fa;
+        padding: 10px;
+        border-bottom: 1px solid #ddd;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        direction: rtl;
+    }
+
+    .navbar-mini-nav {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+
+    .navbar-mini-nav-item {
+        margin-right: 15px;
+    }
+
+    .navbar-mini-nav-link {
+        text-decoration: none;
+        color: #333;
+        font-size: 20px;
+    }
+
+    .navbar-mini-nav-link:hover {
+        color: #007bff;
+    }
+    .active{
+        color: #007bff;
+    }
+</style>
+
+<nav class="navbar-mini">
+    <ul class="navbar-mini-nav" id="navbar-mini-nav">
+        <li class="navbar-mini-nav-item"><a class="navbar-mini-nav-link" href="addonmodules.php?module=zibal">داشبورد</a></li>
+        <li class="navbar-mini-nav-item"><a class="navbar-mini-nav-link" href="addonmodules.php?module=zibal&action=zibal/user">لیست کاربران</a></li>
+        <li class="navbar-mini-nav-item"><a class="navbar-mini-nav-link active" href="addonmodules.php?module=zibal&action=zibal/setting">تنظیمات</a></li>
+    </ul>
+</nav>
 <hr>
 <form style="direction: rtl" class="" method="post"
       action="addonmodules.php?module=zibal&action=zibal/setting">
